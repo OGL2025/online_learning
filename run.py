3f32f1c5706100c019d4533ad214b44f55d691e2
OGL2025 <jessegkopel@gmail.com>
2026-03-23 01:10:32 +1000

    Updated templates, css, and run.py

--- run.py
+++ run.py
@@ -1,20 +1,21 @@
 from app import create_app, db
-from app.models import User, Course, Material, Assignment, Submission, DiscussionPost, Comment, Enrollment # Updated
+from app.models import User, Course, Material, Assignment, Submission, DiscussionPost, Comment, Enrollment
 
 app = create_app()
 
+# Shell context for flask shell command
 @app.shell_context_processor
 def make_shell_context():
     return {
         'db': db, 
-        'User': User, # Changed from Student
+        'User': User, 
         'Course': Course,
         'Material': Material,
         'Assignment': Assignment,
         'Submission': Submission,
         'DiscussionPost': DiscussionPost,
         'Comment': Comment,
-        'Enrollment': Enrollment # New
+        'Enrollment': Enrollment
     }
 
 if __name__ == '__main__':
